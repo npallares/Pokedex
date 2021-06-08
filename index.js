167c738e547e9d65987261b79daa6b9b044350d6
Nicolas <nicolaspallaresdg@gmail.com>
2021-06-08 03:24:34 -0300

    Avance al poder identificar elementos del template

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const $card = document.getElementById("card")
-const $titulo = document.getElementById("titulo")
-const $descripcion = document.getElementById("descripcion")
-const $idPokemon = document.getElementById("idPokemon")
-const $attack = document.getElementById("attack")
-const $defense = document.getElementById("defense")
-const $speed = document.getElementById("speed")
-const $cuadroImagen = document.getElementById("cuadro_imagen")
+const $main=document.getElementById("main")
+const $tituloIndex = document.getElementById("titulo_Index")
+const $imgIndex = document.getElementById("img_index")
+const $template = document.getElementById("template").content
+const $fragment = document.createDocumentFragment();
+
+console.log($main)
+
 
 
 const consultarPokemon = (id) =>{
@@ -19,64 +19,21 @@ const consultarPokemon = (id) =>{
     })
 }
 
-const random =()=>{  
-    let primerId = Math.round(Math.random() *150)
-    consultarPokemon(primerId)
-}
 
-random()
+consultarPokemon(25)
 
 function mostrarPokemon(pokemon){
-
-    /*********  LLAMADO A IMAGEN  **********/
-    let link_img = pokemon.sprites.other.dream_world.front_default;
-    $card.children[0].children[0].setAttribute("src",link_img)    
-
-    /********** LLAMADO A NOMBRE / ID  *********/
-    
     let nombre=pokemon.name.toUpperCase();
-    $titulo.innerHTML=nombre
-
-    console.log(pokemon.order)
-    $idPokemon.innerHTML=`# ${pokemon.order}`
-
-    /********** LLAMADO A DESCRIPCION *********/
-       
-    let link="";
-    link=pokemon.species.url
-    /* console.log(link) */
-
-    fetch(link)
-    .then(function (response){
-            response.json()
-            .then(function(descripcion){
-                /* console.log(descripcion.flavor_text_entries)      */   
-                /* console.log(descripcion.flavor_text_entries[index].language.name) */
-
-                for (let index = 0; index < 200 ; index++) {
-                   
-                    if(descripcion.flavor_text_entries[index].language.name == "es"){
-                        /* console.log(descripcion.flavor_text_entries[index]); */
-                        let description = descripcion.flavor_text_entries[index].flavor_text;
-                        $descripcion.innerHTML=description
-                        index=200;
-                    } 
-                    
-                }
-                // Seteo color de fondo
-                console.log(descripcion.color.name)
-                let bgColor=descripcion.color.name
-                $cuadroImagen.classList.add(`bgcolor-${bgColor}`)
-
-                // Seteo ataque, defensa y velocidad
-                /* console.log(pokemon.stats[1].base_stat) */
-                /* console.log(pokemon.stats) */
-                $attack.innerHTML=pokemon.stats[1].base_stat
-                $defense.innerHTML=pokemon.stats[2].base_stat
-                $speed.innerHTML=pokemon.stats[3].base_stat
-            })
-    }) 
-
-    console.log(pokemon)
+    $tituloIndex.innerHTML=nombre;
+    $imgIndex.setAttribute("src",pokemon.sprites.other.dream_world.front_default)
+    
+    let $clone = document.importNode($template,true)
+    $template.querySelector("img").setAttribute("src",pokemon.sprites.other.dream_world.front_default)
+    $template.querySelector("h1").setAttribute("id","hola");
+    $fragment.appendChild($clone)
+    let imgFrag=$fragment.children[0].querySelector("img")
+    imgFrag.setAttribute("src","hola")
+    console.log(imgFrag)
+    $main.appendChild($fragment)    
     
 }
---
+++ tarjeta.js
@@ -0,0 +1,85 @@
+const $tituloIndex = document.getElementById("card_index")
+const $card = document.getElementById("card")
+const $titulo = document.getElementById("titulo")
+const $descripcion = document.getElementById("descripcion")
+const $idPokemon = document.getElementById("idPokemon")
+const $attack = document.getElementById("attack")
+const $defense = document.getElementById("defense")
+const $speed = document.getElementById("speed")
+
+
+console.log($tituloIndex)
+
+const consultarPokemon = (id) =>{
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(function (response){
+        response.json()
+        .then(function(pokemon){
+            mostrarPokemon(pokemon)
+            
+        })
+    })
+}
+
+const random =()=>{  
+    let primerId = Math.round(Math.random() *150)
+    consultarPokemon(primerId)
+}
+
+random()
+
+function mostrarPokemon(pokemon){
+
+    /*********  LLAMADO A IMAGEN  **********/
+    let link_img = pokemon.sprites.other.dream_world.front_default;
+    $card.children[0].children[0].setAttribute("src",link_img)    
+
+    /********** LLAMADO A NOMBRE / ID  *********/
+    
+    let nombre=pokemon.name.toUpperCase();
+    $titulo.innerHTML=nombre;
+    /* $tituloIndex.innerHTML=nombre; */
+
+    console.log(pokemon.order)
+    $idPokemon.innerHTML=`# ${pokemon.order}`
+
+    /********** LLAMADO A DESCRIPCION *********/
+       
+    let link="";
+    link=pokemon.species.url
+    /* console.log(link) */
+
+    fetch(link)
+    .then(function (response){
+            response.json()
+            .then(function(descripcion){
+                /* console.log(descripcion.flavor_text_entries)      */   
+                /* console.log(descripcion.flavor_text_entries[index].language.name) */
+
+                for (let index = 0; index < 200 ; index++) {
+                   
+                    if(descripcion.flavor_text_entries[index].language.name == "es"){
+                        /* console.log(descripcion.flavor_text_entries[index]); */
+                        let description = descripcion.flavor_text_entries[index].flavor_text;
+                        $descripcion.innerHTML=description
+                        index=200;
+                    } 
+                    
+                }
+                // Seteo color de fondo
+                console.log(descripcion.color.name)
+                let bgColor=descripcion.color.name
+                $cuadroImagen.classList.add(`bgcolor-${bgColor}`)
+
+                // Seteo ataque, defensa y velocidad
+                /* console.log(pokemon.stats[1].base_stat) */
+                /* console.log(pokemon.stats) */
+                $attack.innerHTML=pokemon.stats[1].base_stat
+                $defense.innerHTML=pokemon.stats[2].base_stat
+                $speed.innerHTML=pokemon.stats[3].base_stat
+            })
+    }) 
+
+    console.log(pokemon)
+    
+}
