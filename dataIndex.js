623434308341ee491c8362ede62208329db10e1c
Nicolas <nicolaspallaresdg@gmail.com>
2021-06-08 20:38:46 -0300

    Para Merge

---
+++ dataIndex.js
@@ -0,0 +1,19 @@
+export default function hola(){
+    console.log("hola")
+}
+
+/* const $main=document.getElementById("main")
+
+const data_id=0;
+
+
+    $main.addEventListener("click",(el)=>{
+        el.preventDefault()
+        if(el.target.matches(".button_index")){
+            data_id=
+            data_id=el.target.id
+            console.log(data_id)
+        }
+    })
+ */
+
