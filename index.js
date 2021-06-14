8fa59cd589bbcce966d6a578aecb27bb9dccf7c4
Nicolas <nicolaspallaresdg@gmail.com>
2021-06-14 13:16:22 -0300

    Pokedex V1.0.2

--- index.js
+++ index.js
@@ -1,191 +1,191 @@
-window.addEventListener("load",function(){
-    const $loader = document.getElementById("loader")
-    $loader.classList.add("disp-none")
-})
+window.onload = function () {
+  const $loader = document.getElementById("loader");
+  $loader.classList.add("disp-none");
+};
 
-/* window.onload=function(){
-
-} */
-const $main=document.getElementById("main")
-const $tituloIndex = document.getElementById("titulo_Index")
-const $imgIndex = document.getElementById("img_index")
-const $template = document.getElementById("template").content
+const $main = document.getElementById("main");
+const $tituloIndex = document.getElementById("titulo_Index");
+const $imgIndex = document.getElementById("img_index");
+const $template = document.getElementById("template").content;
 const $fragment = document.createDocumentFragment();
 
 /* export const data_id=0; */
 
 /* console.log($main) */
 
+const consultarPokemon = (id) => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(function (response) {
+    response.json().then(function (pokemon) {
+      mostrarPokemon(pokemon);
+    });
+  });
+};
 
+for (let index = 1; index <= 150; index++) {
 
-const consultarPokemon = (id) =>{
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then(function (response){
-        response.json()
-        .then(function(pokemon){
-            mostrarPokemon(pokemon)
-            
-        })
-    })
-}
+  let idPokemon = index;
+  consultarPokemon(idPokemon);
+  function mostrarPokemon(pokemon) {
+    let link = pokemon.species.url;
 
+    fetch(link).then(function (response) {
+      response.json().then(function (data) {
+        let $clone = document.importNode($template, true);
+        $fragment.appendChild($clone);
 
-for (let index = 0; index <= 150; index++) {
-    /* s */
-    
-    let idPokemon = index;
-    consultarPokemon(idPokemon)
-    function mostrarPokemon(pokemon){
-        
-        let link=pokemon.species.url
-        /* console.log(link) */
+        //Seteo de id para el Section
+        $fragment.children[0].setAttribute("id", data.id);
 
-        fetch(link)
-        .then(function(response){
-            response.json()
-            .then(function(data){
-                
-                let $clone = document.importNode($template,true)
-                $fragment.appendChild($clone)
-                
-                //Seteo de id para el Section
-                $fragment.children[0].setAttribute("id",data.id)
+        //Seteo de id para A Href
+        $fragment.children[0].children[0].setAttribute("id", data.id);
 
-                //Seteo de id para A Href
-                $fragment.children[0].children[0].setAttribute("id",data.id)
-                
-                //Seteo de estilo para Mouse Hover
-                $fragment.children[0].children[0].children[0].classList.add(`hover_index`)
-                
-                //Seteo de color para fondo
-                let color=data.color.name
-                $fragment.children[0].children[0].children[0].classList.add(`bgcolor-${color}`)  
-                
-                // Deteccion y seteo de boton para agregado del DataId 
-                $fragment.children[0].children[0].children[0].children[0].children[1].setAttribute("id",data.id)
-                
-                //Seteo de nombre para Nombre o titulo
-                let h1Frag=$fragment.children[0].querySelector("h1")
-                let nombre=pokemon.name.toUpperCase();                
-                h1Frag.innerHTML=nombre
-                
-                //Seteo de imagen                
-                let imgFrag=$fragment.children[0].children[0].children[0].children[1].children[0]
-                imgFrag.setAttribute("src",pokemon.sprites.other.dream_world.front_default)
-                
-                
-                //Seteo de descripcion para buscar la info en español             
-                let descripcion = $fragment.children[0].querySelector("p")
-                
-                for (let index = 0; index < 200 ; index++) {      
-                    
-                    if(data.flavor_text_entries[index].language.name == "es"){
-                        descripcion.innerHTML = data.flavor_text_entries[index].flavor_text
-                        index = 200
-                    }
-                }
-                
-                //**************Seteo de "0" para el ID a mostrar*********** */
+        //Seteo de estilo para Mouse Hover
+        $fragment.children[0].children[0].children[0].classList.add(
+          `hover_index`
+        );
 
-                let stringDataId = String(data.id)
+        //Seteo de color para fondo
+        let color = data.color.name;
+        $fragment.children[0].children[0].children[0].classList.add(
+          `bgcolor-${color}`
+        );
 
-                if(stringDataId.length == 1){
-                    stringDataId = `00${stringDataId}`
-                }else if(stringDataId.length == 2){
-                    stringDataId = `0${stringDataId}`
-                }
+        // Deteccion y seteo de boton para agregado del DataId
+        $fragment.children[0].children[0].children[0].children[0].children[1].setAttribute(
+          "id",
+          data.id
+        );
 
-                //*****************************ACA*************** */
-                
-                let card = $fragment.children[0] // card
-                let contenedor = card.children[0].children[0] // Contenedor
-                let contenedor_box = contenedor.children[1]
-                let selector = $fragment.children[0].children[0]
-                let descrpcion = contenedor.children[0].children[1]
-                let numeropokemon = contenedor.children[1].children[1]
-                let habilidades = contenedor.children[0].children[2]
-                let descAtaque=contenedor.children[0].children[2].children[0].children[1]
-                let descDefensa=contenedor.children[0].children[2].children[1].children[1]
-                let descVelocidad=contenedor.children[0].children[2].children[2].children[1]
-                
-                let cont = 0
-                
-                /* console.log($fragment.children[0].children[0].children[0].children[1].children[1]) */
-                
-                selector.addEventListener("click",(e)=>{
-                    e.preventDefault()
-                    /* console.log(e.target) */
-                    if(e.target.matches(".button_index") || e.target.matches(".contenedor_index") || e.target.matches(".img_index") || e.target.matches(".titulo_index")){
-                        
-                        if(cont == 0){
-                        /* card.setAttribute("class","card_index alto") */
-                        contenedor.setAttribute("class",`modulo_on bgcolor-black alto`)
+        //Seteo de nombre para Nombre o titulo
+        let h1Frag = $fragment.children[0].querySelector("h1");
+        let nombre = pokemon.name.toUpperCase();
+        h1Frag.innerHTML = nombre;
 
-                        descrpcion.setAttribute("class","descripcion")
-                        
-                        contenedor_box.setAttribute("class","box_on")
+        //Seteo de imagen
+        let imgFrag =
+          $fragment.children[0].children[0].children[0].children[1].children[0];
+        imgFrag.setAttribute(
+          "src",
+          pokemon.sprites.other.dream_world.front_default
+        );
 
-                        numeropokemon.setAttribute("class","idPokemonIndex")
-                        numeropokemon.innerHTML="# "+stringDataId
-                        
+        //Seteo de descripcion para buscar la info en español
+        let descripcion = $fragment.children[0].querySelector("p");
 
-                        habilidades.setAttribute("class","habilidades")
+        for (let index = 0; index < 200; index++) {
+          if (data.flavor_text_entries[index].language.name == "es") {
+            descripcion.innerHTML = data.flavor_text_entries[index].flavor_text;
+            index = 200;
+          }
+        }
 
-                        descAtaque.innerHTML="Ataque " + pokemon.stats[1].base_stat 
-                        
-                        descDefensa.innerHTML="Defensa " + pokemon.stats[2].base_stat
-                        
-                        descVelocidad.innerHTML="Velocidad " + pokemon.stats[3].base_stat
+        //**************Seteo de "0" para el ID a mostrar*********** */
 
+        let stringDataId = String(data.id);
 
-                        cont=1
+        if (stringDataId.length == 1) {
+          stringDataId = `00${stringDataId}`;
+        } else if (stringDataId.length == 2) {
+          stringDataId = `0${stringDataId}`;
+        }
 
-                        } else{
-                            contenedor.setAttribute("class",`modulo_index hover_index bgcolor-${color}`)
-                            descrpcion.setAttribute("class","disp-none")
-                            contenedor_box.setAttribute("class","box")
-                            numeropokemon.setAttribute("class","disp-none")
-                            habilidades.setAttribute("class","habilidades disp-none")
-                            
-                            cont=0
-                        }
-                    } else {
-                        card.setAttribute("class","card_index")
-                    }
-                })
-                
-                //Agregado de $fragment al html
-                $main.appendChild($fragment)
-                
-                let a = $main.querySelector("a")
-                
-                
-            })
-        })
-        /* a */
-    }
+        //*****************************ACA*************** */
+
+        let card = $fragment.children[0]; // card
+        let contenedor = card.children[0].children[0]; // Contenedor
+        let contenedor_box = contenedor.children[1];
+        let selector = $fragment.children[0].children[0];
+        let descrpcion = contenedor.children[0].children[1];
+        let numeropokemon = contenedor.children[1].children[1];
+        let habilidades = contenedor.children[0].children[2];
+        let descAtaque =
+          contenedor.children[0].children[2].children[0].children[1];
+        let descDefensa =
+          contenedor.children[0].children[2].children[1].children[1];
+        let descVelocidad =
+          contenedor.children[0].children[2].children[2].children[1];
+
+        let cont = 0;
+
+        /* console.log($fragment.children[0].children[0].children[0].children[1].children[1]) */
+
+        selector.addEventListener("click", (e) => {
+          e.preventDefault();
+          /* console.log(e.target) */
+          if (
+            e.target.matches(".button_index") ||
+            e.target.matches(".contenedor_index") ||
+            e.target.matches(".img_index") ||
+            e.target.matches(".titulo_index")
+          ) {
+            if (cont == 0) {
+              /* card.setAttribute("class","card_index alto") */
+              contenedor.setAttribute("class", `modulo_on bgcolor-black alto`);
+
+              descrpcion.setAttribute("class", "descripcion");
+
+              contenedor_box.setAttribute("class", "box_on");
+
+              numeropokemon.setAttribute("class", "idPokemonIndex");
+              numeropokemon.innerHTML = "# " + stringDataId;
+
+              habilidades.setAttribute("class", "habilidades");
+
+              descAtaque.innerHTML = "Ataque " + pokemon.stats[1].base_stat;
+
+              descDefensa.innerHTML = "Defensa " + pokemon.stats[2].base_stat;
+
+              descVelocidad.innerHTML =
+                "Velocidad " + pokemon.stats[3].base_stat;
+
+              cont = 1;
+            } else {
+              contenedor.setAttribute(
+                "class",
+                `modulo_index hover_index bgcolor-${color}`
+              );
+              descrpcion.setAttribute("class", "disp-none");
+              contenedor_box.setAttribute("class", "box");
+              numeropokemon.setAttribute("class", "disp-none");
+              habilidades.setAttribute("class", "habilidades disp-none");
+
+              cont = 0;
+            }
+          } else {
+            card.setAttribute("class", "card_index");
+          }
+        });
+
+        //Agregado de $fragment al html
+        $main.appendChild($fragment);
+
+        let a = $main.querySelector("a");
+      });
+    });
+    /* a */
+  }
 }
 
 /**********BUSCADOR*************/
 
-function serchFilters(input, cardindex){
-
-    document.addEventListener("keyup",(e)=>{
-        
-        if(e.target.matches(input)){
-        
-            document.querySelectorAll(cardindex).forEach((el)=>{
-                if(!el.children[0].children[0].children[0].children[0].textContent.toLowerCase().includes(e.target.value)){
-                    el.classList.add("disp-none")
-                    
-                    }else {
-                        el.classList.remove("disp-none")
-                }
-            })
-            
-            if(e.key == "Escape") e.target.value="";
+function serchFilters(input, cardindex) {
+  document.addEventListener("keyup", (e) => {
+    if (e.target.matches(input)) {
+      document.querySelectorAll(cardindex).forEach((el) => {
+        if (
+          !el.children[0].children[0].children[0].children[0].textContent
+            .toLowerCase()
+            .includes(e.target.value)
+        ) {
+          el.classList.add("disp-none");
+        } else {
+          el.classList.remove("disp-none");
         }
-    })
+      });
+
+      if (e.key == "Escape") e.target.value = "";
+    }
+  });
 }
 
-serchFilters(".card-filter",".card_index")
+serchFilters(".card-filter", ".card_index");
